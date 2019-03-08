496b1c64bcf14d74fd5d048fcb66f6601745ab26
Omar Orozco <omar@MacBook-Air-de-Omar.local>
2019-03-08 14:42:27 -0300

    actualizacion scrapy

--- comparagrow/settings.py
+++ comparagrow/settings.py
@@ -123,7 +123,7 @@ EMAIL_PORT = 587
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
- DATABASES = {
+DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.postgresql_psycopg2',
          'NAME': 'comparagrow',
@@ -139,7 +139,7 @@ EMAIL_PORT = 587
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
-# }
+}
 
 # DATABASES = {
 #    'default': {
