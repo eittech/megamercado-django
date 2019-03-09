484d0af4f19b0ef074a68473954a15bbc45886d5
Omar Orozco <omar@MacBook-Air-de-Omar.local>
2019-03-09 16:14:19 -0300

    tag return image favorite

--- comparagrow/settings.py
+++ comparagrow/settings.py
@@ -123,23 +123,23 @@ EMAIL_PORT = 587
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#          'NAME': 'comparagrowtest',
-#          'USER': 'postgres',
-#          'PASSWORD': 'pazverde123',
-#          'HOST': 'localhost',
-#          'PORT': '',
-#      }
-#  }
-
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'comparagrowtest',
+         'USER': 'postgres',
+         'PASSWORD': 'pazverde123',
+         'HOST': 'localhost',
+         'PORT': '',
+     }
+ }
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
 
 # DATABASES = {
 #    'default': {
