fa33e752d3ef939b7fab24dc0b16028fa2ec856c
Omar Orozco <omar@MacBook-Air-de-Omar.local>
2019-03-18 20:08:20 -0300

    scarping

--- comparagrow/settings.py
+++ comparagrow/settings.py
@@ -124,23 +124,23 @@ EMAIL_PORT = 587
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#          'NAME': 'comparagrow',
-#          'USER': 'postgres',
-#          'PASSWORD': 'pazverde123',
-#          'HOST': 'localhost',
-#          'PORT': '',
-#      }
-#  }
-
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite36'),
-   }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'comparagrow',
+         'USER': 'postgres',
+         'PASSWORD': 'pazverde123',
+         'HOST': 'localhost',
+         'PORT': '',
+     }
+ }
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite36'),
+#    }
+# }
 
 # DATABASES = {
 #    'default': {
