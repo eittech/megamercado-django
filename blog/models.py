3532c514b49d3f6f7a249acb85328549a02b487f
Omar Orozco <omar@MacBook-Air-de-Omar.local>
2019-04-20 01:28:15 -0400

    mode

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.text import slugify
 
 # Create your models here.
 class Blog(models.Model):
@@ -7,10 +7,16 @@ class Blog(models.Model):
     slug = models.SlugField()
     image = models.ImageField(upload_to="assets/blog/",blank=True,null=True)
     url = models.URLField(verbose_name="URL",max_length=200,blank=True)
+    source = models.CharField(max_length=300,blank=True,null=True)
+    page_source = models.URLField(verbose_name="Feed Pagina",max_length=200,blank=True)
     description = models.TextField(verbose_name="Descripcion",blank=True)
+    description_short = models.TextField(verbose_name="Descripcion",blank=True,null=True)
     fecha = models.DateTimeField(verbose_name="Fecha de publicacion",auto_now=True)
     def __str__(self):
         return self.title
     class Meta:
         verbose_name = "Blog"
         unique_together = (('slug',))
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Blog, self).save(*args, **kwargs)
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-
+from django.core.paginator import Paginator
 # Create your views here.
 from blog.models import *
 
 def lists(request):
     post =  Blog.objects.all()
-    return render(request, 'comparagrow/porto/blog.html', {'post':post})
+    paginator = Paginator(post, 10) # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+    return render(request, 'comparagrow/porto/blog.html', {'post':posts})
 
 def post(request,slug):
     post =  Blog.objects.get(slug=slug)
