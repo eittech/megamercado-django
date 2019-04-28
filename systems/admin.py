b8c68654016ca6fe59f4d71a10b85c82a8b9ec5b
Omar Orozco <omar@MacBook-Air-de-Omar.local>
2019-04-28 19:37:45 -0400

    register activity

--- systems/admin.py
+++ systems/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(RegisterActivitySystem)
+class RegisterActivitySystemAdmin(admin.ModelAdmin):
+    list_filter = ('type',('user',admin.RelatedOnlyFieldListFilter))
+    list_display = ('type', 'user','datet')
+    date_hierarchy = 'datet'
+    # readonly_fields = ['category','category_temp','photo']
+    # search_fields = ['name']
+admin.site.register(RegisterActivitySystem,RegisterActivitySystemAdmin)
 admin.site.register(ContactMessage)
