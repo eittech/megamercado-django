7ec3486f6bd02162ed686dde79032f436784bcd2
Omar Orozco <omar@MacBook-Air-de-Omar.local>
2019-05-27 01:54:26 -0400

    saviagrowshop.py

--- scraper/scraper/spiders/saviagrowshop.py
+++ scraper/scraper/spiders/saviagrowshop.py
@@ -90,13 +90,13 @@ class ProductSpider(scrapy.Spider):
             name_category = 'otros'
 
         category = None
-        category_tags = CategoryTags.objects.filter(tag__icontains=name_category).first()
-        if category_tags is None:
-            category = None
-        else:
-            category = category_tags.category
+        # category_tags = CategoryTags.objects.filter(tag=a.lower()).filter(category__isnull=False).order_by('-category__level').first()
+        # if category_tags is None:
+        #     category = None
+        # else:
+        #     category = category_tags.category
 
-        if category is not None:
+        if True:
             name_category = response.meta['name_category_safe']
             product = response.css("div.woocommerce")
             name = product.xpath('.//div[@class="summary entry-summary"]/h1[@class="product_title entry-title"]/text()').re_first('\w.*')
@@ -124,94 +124,111 @@ class ProductSpider(scrapy.Spider):
                 total = int(total)
             except:
                 total = None
+            Product_exist = Product.objects.filter(url=url).first()
+            print(total)
+            if Product_exist:
+                Product_object = Product_exist
+                if total:
+                    Product_object.total = total
 
-            Product_object = Product()
-            if name:
-                Product_object.name = name
-                print('name:')
-                print(name)
-            if shop_id:
-                Product_object.shop = shop_id
-                print('shop:')
-                print(shop_id)
-            if reference:
-                Product_object.reference = reference
-                print('reference:')
-                print(reference)
-            if brand:
-                Product_object.brand = brand
-                print('marca:')
-                print(brand)
-            if url:
-                Product_object.url = url
-                print('url:')
-                print(url)
-            if category_temp:
-                print('category_temp:')
-                print(category_temp)
-                Product_object.category_temp = category_temp
-            if description:
-                Product_object.description = description
-                print('description:')
-                print(description)
-
-            if category is not None:
-                Product_object.category = category
-                print('category:')
-                print(category)
-            if total:
-                print('total:')
-                print(total)
-                Product_object.total = total
-            else:
-                Product_object.total = 0
-
-            Product_object.price = 0
-            Product_object.tax = 0
-
-            print(Product_object)
-            try:
-                Product_object.save()
-                print("*****************")
-                print("*****************")
-                print("*****************")
-                print("se guardo con exito")
-                product_error = False
-            except:
-                product_error = True
-                print("*****************")
-                print("*****************")
-                print("*****************")
-                print("No se pudo guardar el producto")
-
-            if Product_object.id:
-                list_img_t = response.xpath('.//figure[@class="woocommerce-product-gallery__wrapper"]')
-
-                print("*****************")
-                print("*****************")
-                print("*****************")
-                print(list_img_t)
-                list_img = list_img_t.css('img')
-                print(list_img)
-                headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
-                contador = 0
-                for li_img in list_img:
-                    print(li_img)
-                    contador = contador + 1
-                    img_url = li_img.xpath('@src').re_first('\w.*')
-                    print(img_url)
-                    # img_url = response.css('img#bigpic').xpath('@src').get()
-                    name = str(Product_object.id) +'_' + str(contador) + '.jpg'
-
-                    producto_image = ProductImage()
-                    producto_image.product = Product_object
-
-                    req = Request(url=img_url, headers=headers)
-                    response = urlopen(req)
-
-                    io = BytesIO(response.read())
-                    producto_image.image.save(name, File(io))
-
-                    producto_image.save()
-        else:
-            print('no existe la categoria')
+                try:
+                    if float(total) > 0:
+                        Product_object.save()
+                        print("Se actualizo el precio")
+                        product_error = False
+                    else:
+                        print("No Se actualizo el precio")
+                except:
+                    product_error = True
+                    print("No se actualizo el precio")
+        #     else:
+        #         Product_object = Product()
+        #         if name:
+        #             Product_object.name = name
+        #             print('name:')
+        #             print(name)
+        #         if shop_id:
+        #             Product_object.shop = shop_id
+        #             print('shop:')
+        #             print(shop_id)
+        #         if reference:
+        #             Product_object.reference = reference
+        #             print('reference:')
+        #             print(reference)
+        #         if brand:
+        #             Product_object.brand = brand
+        #             print('marca:')
+        #             print(brand)
+        #         if url:
+        #             Product_object.url = url
+        #             print('url:')
+        #             print(url)
+        #         if category_temp:
+        #             print('category_temp:')
+        #             print(category_temp)
+        #             Product_object.category_temp = category_temp
+        #         if description:
+        #             Product_object.description = description
+        #             print('description:')
+        #             print(description)
+        #
+        #         if category is not None:
+        #             Product_object.category = category
+        #             print('category:')
+        #             print(category)
+        #         if total:
+        #             print('total:')
+        #             print(total)
+        #             Product_object.total = total
+        #         else:
+        #             Product_object.total = 0
+        #
+        #         Product_object.price = 0
+        #         Product_object.tax = 0
+        #
+        #         print(Product_object)
+        #         try:
+        #             Product_object.save()
+        #             print("*****************")
+        #             print("*****************")
+        #             print("*****************")
+        #             print("se guardo con exito")
+        #             product_error = False
+        #         except:
+        #             product_error = True
+        #             print("*****************")
+        #             print("*****************")
+        #             print("*****************")
+        #             print("No se pudo guardar el producto")
+        #
+        #         if Product_object.id:
+        #             list_img_t = response.xpath('.//figure[@class="woocommerce-product-gallery__wrapper"]')
+        #
+        #             print("*****************")
+        #             print("*****************")
+        #             print("*****************")
+        #             print(list_img_t)
+        #             list_img = list_img_t.css('img')
+        #             print(list_img)
+        #             headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+        #             contador = 0
+        #             for li_img in list_img:
+        #                 print(li_img)
+        #                 contador = contador + 1
+        #                 img_url = li_img.xpath('@src').re_first('\w.*')
+        #                 print(img_url)
+        #                 # img_url = response.css('img#bigpic').xpath('@src').get()
+        #                 name = str(Product_object.id) +'_' + str(contador) + '.jpg'
+        #
+        #                 producto_image = ProductImage()
+        #                 producto_image.product = Product_object
+        #
+        #                 req = Request(url=img_url, headers=headers)
+        #                 response = urlopen(req)
+        #
+        #                 io = BytesIO(response.read())
+        #                 producto_image.image.save(name, File(io))
+        #
+        #                 producto_image.save()
+        # else:
+        #     print('no existe la categoria')
