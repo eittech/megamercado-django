c1a690e169854f81572e0dce9bcade669b06842e
Giulianne17 <gvtr96@gmail.com>
2019-06-21 12:11:27 +0200

    Se crearon los casos de prueba para el modelo CategoryProduct

--- products/tests.py
+++ products/tests.py
@@ -1289,3 +1289,116 @@ class ProductTestCase(TestCase):
         }
         form = ProductForm(data=form_data)
         self.assertFalse(form.is_valid())
+
+class CategoryProductTestCase(TestCase):
+    ''' Pruebas para la tabla de CategoryProduct '''
+
+    def setUp(self):
+        self.categoria = Category.objects.create(
+            id_category= "1",
+            name="Nombre",
+            description= "Description",
+            level_depth= 0,
+            active= "True",
+            date_add= timezone.now(),
+            date_upd= timezone.now(),
+            position= 1,
+            is_root_category= "False"
+        )
+        self.shopgroup = ShopGroup.objects.create(
+            id_shop_group="2",
+            name="Nombre grupo",
+            share_order= "True",
+            share_stock= "False",
+            active= "True",
+            deleted= "False")
+        self.shop= Shop.objects.create(
+            id_shop= "1",
+            id_shop_group= self.shopgroup,
+            name="Nombre"
+        )
+        self.producto=Product.objects.create(
+            id_product="1",
+            id_category_default= self.categoria,
+            id_shop_default= self.shop,
+            name="Nombre",
+            on_sale="True",
+            online_only= "True",
+            quantity= 0,
+            minimal_quantity= 0,
+            price= 1,
+            wholesale_price= 0,
+            unit_price_ratio= 0,
+            additional_shipping_cost=1,
+            width=1,
+            height=1,
+            depth=1,
+            weight=1,
+            out_of_stock= 0,
+            available_for_order="True",
+            available_date= "2018-07-29",
+            condition= "new",
+            visibility= "everywhere",
+            is_virtual="True",
+            date_add= timezone.now(),
+            date_upd= timezone.now()
+        )
+
+    '''Caso de prueba para verificar que se crea un Producto'''
+    def test_categoryproduct_crear(self):
+        form_data = {
+            'id_product':self.producto.id_product,
+            'id_category' : self.categoria.id_category,
+            'position':1
+        }
+        form = CategoryProductForm(data=form_data)
+        form.save()
+        group1 = CategoryProduct.objects.get(id_product = "1")
+        self.assertEqual(group1.id_product.name, "Nombre")
+    
+    '''Caso de prueba para verificar que se elimina un Producto'''
+    def test_categoryproduct_eliminar(self):
+        form_data = {
+            'id_product':self.producto.id_product,
+            'id_category' : self.categoria.id_category,
+            'position':1
+        }
+        form = CategoryProductForm(data=form_data)
+        form.save()
+        group1 = CategoryProduct.objects.get(id_product = "1").delete()
+        try:
+            group1 = CategoryProduct.objects.get(id_product = "1")
+        except:
+            pass
+
+    '''Caso de prueba para verificar que se crea una categoria de un  
+        producto sin foranea'''
+    def test_categoryproduct_sin_foranea(self):
+        form_data = {
+            'id_category' : self.categoria.id_category,
+            'position':1
+        }
+        form = AttributeGroupShopForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    '''Caso de prueba para verificar si se crea una categoria de un  
+        producto con foranea mala'''
+    def test_categoryproduct_foranea_mala(self):
+        form_data = {
+            'id_product':"mala",
+            'id_category' : self.categoria.id_category,
+            'position':1
+        }
+        form = AttributeGroupShopForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    '''Caso de prueba para verificar si se crea una categoria de un  
+        producto con position negativo'''
+    def test_categoryproduct_position_negativo(self):
+        form_data = {
+            'id_product':self.producto.id_product,
+            'id_category' : self.categoria.id_category,
+            'position':-1
+        }
+        form = AttributeGroupShopForm(data=form_data)
+        self.assertFalse(form.is_valid())
