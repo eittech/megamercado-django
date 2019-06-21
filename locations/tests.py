f19899c4e59440b5637d3668d9143087d93c4b4b
Giulianne17 <gvtr96@gmail.com>
2019-06-21 16:57:01 +0200

    Se crearon las pruebas al modelo de Country

--- locations/tests.py
+++ locations/tests.py
@@ -61,3 +61,102 @@ class ZoneTestCase(TestCase):
         }
         form = ZoneForm(data=form_data)
         self.assertFalse(form.is_valid())
+
+class CountryTestCase(TestCase):
+    ''' Pruebas para la tabla de ShopGroup '''
+
+    def setUp(self):
+        self.zone= Zone.objects.create(
+            id_zone= "1",
+            name= "Nombre",
+            active= "True"
+        )
+
+    '''Caso de prueba para verificar que se crea un pais'''
+    def test_country_crear(self):
+        form_data = {
+            'id_country' : "1",
+            'id_zone': self.zone.id_zone,
+            'name' : "Nombre",
+            'iso_code': "626",
+            'call_prefix': 23,
+            'zip_code_format' : "www"
+        }
+        form = CountryForm(data=form_data)
+        form.save()
+        shop1 = Country.objects.get(name = "Nombre")
+        self.assertEqual(shop1.name, "Nombre")
+    
+    '''Caso de prueba para verificar que se elimina un pais'''
+    def test_country_eliminar(self):
+        form_data = {
+            'id_country' : "1",
+            'id_zone': self.zone.id_zone,
+            'name' : "Nombre",
+            'iso_code': "626",
+            'call_prefix': 23,
+            'zip_code_format' : "www"
+        }
+        form = CountryForm(data=form_data)
+        form.save()
+        shop1 = Country.objects.get(name = "Nombre").delete()
+        try:
+            shop1 = Country.objects.get(name = "Nombre")
+        except:
+            pass
+
+    '''Caso de prueba para verificar que se edita un pais'''
+    def test_country_editar(self):
+        form_data = {
+            'id_country' : "1",
+            'id_zone': self.zone.id_zone,
+            'name' : "Nombre",
+            'iso_code': "626",
+            'call_prefix': 23,
+            'zip_code_format' : "www"
+        }
+        form = CountryForm(data=form_data)
+        form.save()
+        shop1 = Country.objects.get(name = "Nombre")
+        shop1.name="Change"
+        shop1.save()
+        shop1 = Country.objects.get(name = "Change")
+        self.assertEqual(shop1.name, "Change")
+
+    '''Caso de prueba para verificar si se crea un pais sin nombre'''
+    def test_country_sin_name(self):
+        form_data = {
+            'id_country' : "1",
+            'id_zone': self.zone.id_zone,
+            'name' : "",
+            'iso_code': "626",
+            'call_prefix': 23,
+            'zip_code_format' : "www"
+        }
+        form = CountryForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    '''Caso de prueba para verificar si se crea un pais sin foranea'''
+    def test_country_sin_foranea(self):
+        form_data = {
+            'id_country' : "1",
+            'name' : "Nombre",
+            'iso_code': "626",
+            'call_prefix': 23,
+            'zip_code_format' : "www"
+        }
+        form = CountryForm(data=form_data)
+        self.assertFalse(form.is_valid())
+    
+    '''Caso de prueba para verificar si se crea un pais con foranea mala'''
+    def test_country_foranea_mala(self):
+        form_data = {
+            'id_country' : "1",
+            'id_zone': "mala",
+            'name' : "Nombre",
+            'iso_code': "626",
+            'call_prefix': 23,
+            'zip_code_format' : "www"
+        }
+        form = CountryForm(data=form_data)
+        self.assertFalse(form.is_valid())
