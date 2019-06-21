79401d9d71d518de9aa4b4824c640f53be9d7b45
Giulianne17 <gvtr96@gmail.com>
2019-06-21 11:11:26 +0200

    Se crearon los formularios de los modelos restantes de los productos.

--- products/forms.py
+++ products/forms.py
@@ -118,3 +118,133 @@ class CategoryGroupForm(forms.ModelForm):
         '''
         model = CategoryGroup
         fields = '__all__'
+
+class ProductForm(forms.ModelForm):
+    '''
+        Aqui se implementa el formulario para la creacion
+        de nuevos Product
+    '''
+    class Meta:
+        '''
+            Aqui se especifica que datos se tienen que incluir en
+            el formulario
+        '''
+        model = Product
+        fields = '__all__'
+
+class CategoryProductForm(forms.ModelForm):
+    '''
+        Aqui se implementa el formulario para la creacion
+        de nuevos Category Product 
+    '''
+    class Meta:
+        '''
+            Aqui se especifica que datos se tienen que incluir en
+            el formulario
+        '''
+        model = CategoryProduct
+        fields = '__all__'
+
+class CategoryShopForm(forms.ModelForm):
+    '''
+        Aqui se implementa el formulario para la creacion
+        de nuevos Category Shop
+    '''
+    class Meta:
+        '''
+            Aqui se especifica que datos se tienen que incluir en
+            el formulario
+        '''
+        model = CategoryShop
+        fields = '__all__'
+
+class ProductAttributeForm(forms.ModelForm):
+    '''
+        Aqui se implementa el formulario para la creacion
+        de nuevos Product Attribute
+    '''
+    class Meta:
+        '''
+            Aqui se especifica que datos se tienen que incluir en
+            el formulario
+        '''
+        model = ProductAttribute
+        fields = '__all__'
+
+class ImageForm(forms.ModelForm):
+    '''
+        Aqui se implementa el formulario para la creacion
+        de nuevos Image
+    '''
+    class Meta:
+        '''
+            Aqui se especifica que datos se tienen que incluir en
+            el formulario
+        '''
+        model = Image
+        fields = '__all__'
+
+class ProductAttributeCombinationForm(forms.ModelForm):
+    '''
+        Aqui se implementa el formulario para la creacion
+        de nuevos Product Attribute Combination
+    '''
+    class Meta:
+        '''
+            Aqui se especifica que datos se tienen que incluir en
+            el formulario
+        '''
+        model = ProductAttributeCombination
+        fields = '__all__'
+
+class ProductAttributeImageForm(forms.ModelForm):
+    '''
+        Aqui se implementa el formulario para la creacion
+        de nuevos Product Attribute Image
+    '''
+    class Meta:
+        '''
+            Aqui se especifica que datos se tienen que incluir en
+            el formulario
+        '''
+        model = ProductAttributeImage
+        fields = '__all__'
+
+class ProductAttributeShopForm(forms.ModelForm):
+    '''
+        Aqui se implementa el formulario para la creacion
+        de nuevos Product Attribute Shop
+    '''
+    class Meta:
+        '''
+            Aqui se especifica que datos se tienen que incluir en
+            el formulario
+        '''
+        model = ProductAttributeShop
+        fields = '__all__'
+
+class ProductShopForm(forms.ModelForm):
+    '''
+        Aqui se implementa el formulario para la creacion
+        de nuevos Product Shop
+    '''
+    class Meta:
+        '''
+            Aqui se especifica que datos se tienen que incluir en
+            el formulario
+        '''
+        model = ProductShop
+        fields = '__all__'
+    
+class AttributeImpactForm(forms.ModelForm):
+    '''
+        Aqui se implementa el formulario para la creacion
+        de nuevos Attribute Impact
+    '''
+    class Meta:
+        '''
+            Aqui se especifica que datos se tienen que incluir en
+            el formulario
+        '''
+        model = AttributeImpact
+        fields = '__all__'
