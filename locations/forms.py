db2a827fdcb043ba6b6e96c7f422362371036f67
Giulianne17 <gvtr96@gmail.com>
2019-06-21 16:59:32 +0200

    Creados los formularios de los modelos de locations

---
+++ locations/forms.py
@@ -0,0 +1,68 @@
+from django import forms
+from .models import *
+
+
+class ZoneForm(forms.ModelForm):
+    '''
+        Aqui se implementa el formulario para la creacion
+        de nuevos Zone
+    '''
+    class Meta:
+        '''
+            Aqui se especifica que datos se tienen que incluir en
+            el formulario
+        '''
+        model = Zone
+        fields = '__all__'
+
+class CountryForm(forms.ModelForm):
+    '''
+        Aqui se implementa el formulario para la creacion
+        de nuevos Country
+    '''
+    class Meta:
+        '''
+            Aqui se especifica que datos se tienen que incluir en
+            el formulario
+        '''
+        model = Country
+        fields = '__all__'
+
+class CountryShopForm(forms.ModelForm):
+    '''
+        Aqui se implementa el formulario para la creacion
+        de nuevos CountryShop
+    '''
+    class Meta:
+        '''
+            Aqui se especifica que datos se tienen que incluir en
+            el formulario
+        '''
+        model = CountryShop
+        fields = '__all__'
+
+class StateForm(forms.ModelForm):
+    '''
+        Aqui se implementa el formulario para la creacion
+        de nuevos State
+    '''
+    class Meta:
+        '''
+            Aqui se especifica que datos se tienen que incluir en
+            el formulario
+        '''
+        model = State
+        fields = '__all__'
+
+class AddressForm(forms.ModelForm):
+    '''
+        Aqui se implementa el formulario para la creacion
+        de nuevos Address
+    '''
+    class Meta:
+        '''
+            Aqui se especifica que datos se tienen que incluir en
+            el formulario
+        '''
+        model = Address
+        fields = '__all__'
